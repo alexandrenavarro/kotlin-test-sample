2a2f6443a8eb15d6805e889f83bf180be3545c36
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2018-12-19 23:35:00 +0100

    Update pom.xml

--- src/test/java/com/github/anavarro/kotlintestsample/CalculatorJunit5Test.java
+++ src/test/java/com/github/anavarro/kotlintestsample/CalculatorJunit5Test.java
@@ -2,14 +2,28 @@ package com.github.anavarro.kotlintestsample;
 
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorJunit5Test {
 
+
+    @Nested
+    class TestContext1 {
+
+        @Test
+        public void addSameValuesLevel2() {
+            final Calculator calculator = new Calculator();
+            int add = calculator.add(1, 1);
+            Assertions.assertThat(add).isEqualTo(1 * 2);
+        }
+    }
+
     @Test
-    public void addSameValues() {
+    public void addSameValuesLevel1() {
         final Calculator calculator = new Calculator();
         int add = calculator.add(1, 1);
+
         Assertions.assertThat(add).isEqualTo(1 * 2);
     }
 
